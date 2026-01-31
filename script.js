57987bcd227e11ed525761cf5b050b6e97ef57a4
MrBhavesh24 <125347014+MrBhavesh24@users.noreply.github.com>
2026-01-31 15:16:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-function submitForm(event){
-  event.preventDefault();
-  alert("Form submitted! Seller will contact you via WhatsApp/email.");
-}
+const products = [
+  {
+    id: 1,
+    name: "Men Printed Shirt",
+    price: 999,
+    category: "men",
+    image: "https://images.unsplash.com/photo-1521334884684-d80222895322",
+    seller: "Trendy Male"
+  },
+  {
+    id: 2,
+    name: "Men Combo Pack",
+    price: 1499,
+    category: "combo",
+    image: "https://images.unsplash.com/photo-1512436991641-6745cdb1723f",
+    seller: "Surat Combo Hub"
+  }
+];
